cfefea27571f2b198581421cc2d04c41a573b02b
DeepEyeCrypto <nazzo.np@gmail.com>
2025-03-14 00:37:38 +0530

    Update Mac-theme.sh

--- Mac-theme.sh
+++ Mac-theme.sh
@@ -9,167 +9,230 @@ C="\033[1;36m"
 W="\033[0m"
 
 # Configuration
-STYLE=${1:-3}  # Default to macOS theme
-BASE_URL="https://github.com/sabamdarif/termux-desktop/raw/setup-files/setup-files/xfce/look_"
-WALLPAPER_DIR="$PREFIX/share/backgrounds"
-ICON_DIR="$HOME/.icons"
 THEME_DIR="$HOME/.themes"
+ICON_DIR="$HOME/.icons"
+WALLPAPER_DIR="$PREFIX/share/backgrounds/xfce"
+FONT_DIR="$HOME/.fonts"
 
-# Icon Packs
-ICON_PACKS=(
-    "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/master.tar.gz"
-    "https://github.com/numixproject/numix-icon-theme/archive/master.tar.gz"
-    "https://github.com/keeferrourke/la-capitaine-icon-theme/archive/master.tar.gz"
-    "https://github.com/vinceliuice/Tela-icon-theme/archive/master.tar.gz"
-    "https://github.com/daniruiz/flat-remix/archive/master.tar.gz"
+# Theme Options
+declare -A THEMES=(
+    [1]="WhiteSur-Dark"
+    [2]="McMojave"
+    [3]="Cupertino"
+    [4]="Monterey"
+    [5]="Ventura"
 )
 
-# macOS Wallpapers
-MACOS_WALLPAPERS=(
+# Resources
+declare -A THEME_REPO=(
+    ["WhiteSur"]="https://github.com/vinceliuice/WhiteSur-gtk-theme/archive/master.tar.gz"
+    ["McMojave"]="https://github.com/paullinuxthemer/McMojave-theme/archive/master.zip"
+    ["Cupertino"]="https://github.com/alexaylwin/Cupertino-gtk-theme/archive/refs/heads/master.zip"
+    ["Monterey"]="https://github.com/vinceliuice/Monterey-kde/archive/master.tar.gz"
+    ["Ventura"]="https://github.com/3ximus/macos-ventura-theme/archive/refs/heads/main.zip"
+)
+
+declare -A ICON_REPO=(
+    ["WhiteSur"]="https://github.com/vinceliuice/WhiteSur-icon-theme/archive/master.tar.gz"
+    ["McMojave"]="https://github.com/zayronxio/McMojave-circle/archive/master.zip"
+    ["Cupertino"]="https://github.com/alexaylwin/Cupertino-mocha-icons/archive/refs/heads/master.zip"
+)
+
+MAC_WALLPAPERS=(
+    "https://4kwallpapers.com/images/wallpapers/macos-monterey-abstract-wwdc-2021-5k-7680x4320-2353.jpg"
+    "https://4kwallpapers.com/images/wallpapers/macos-ventura-abstract-stock-dark-mode-5k-7680x4320-7001.jpeg"
     "https://4kwallpapers.com/images/wallpapers/macos-big-sur-apple-layers-fluidic-colorful-wwdc-stock-4096x2304-1455.jpg"
-    "https://4kwallpapers.com/images/wallpapers/macos-fusion-8k-7680x4320-12482.jpg"
     "https://4kwallpapers.com/images/wallpapers/macos-sonoma-6016x6016-11577.jpeg"
-    "https://4kwallpapers.com/images/wallpapers/macos-sonoma-6016x6016-11576.jpeg"
-    "https://4kwallpapers.com/images/wallpapers/sierra-nevada-mountains-macos-high-sierra-mountain-range-5120x2880-8674.jpg"
 )
 
-initial_setup() {
-    echo -e "${C}[*] Starting initial setup...${W}"
-    
-    # Update packages
-    echo -e "${G}[+] Updating packages...${W}"
-    pkg update -y && pkg upgrade -y
-    
-    # Setup storage
-    echo -e "${G}[+] Setting up storage...${W}"
-    termux-setup-storage
-    echo -e "${Y}[!] Please allow storage permission when prompted!${W}"
-    
-    # Install X11 and core components
-    echo -e "${G}[+] Installing X11 components...${W}"
-    pkg install x11-repo -y
-    pkg install termux-x11-nightly -y
-    pkg install pulseaudio -y
-    
-    # Install desktop environment
-    echo -e "${G}[+] Installing XFCE4...${W}"
-    pkg install xfce4 -y
-    
-    # Install additional software
-    echo -e "${G}[+] Installing applications...${W}"
-    pkg install tur-repo -y
-    pkg install firefox chromium code-oss git wget -y
-    
-    # Install theme dependencies
-    echo -e "${G}[+] Installing theme dependencies...${W}"
-    pkg install zsh -y
+show_menu() {
+    clear
+    echo -e "${C}┌───────────────────────────────────┐"
+    echo -e "│ Termux macOS Theme Suite v2.0 by ejaj ali    │"
+    echo -e "└───────────────────────────────────┘${W}"
+    echo -e "${B}Select macOS Theme:${W}"
+    for key in "${!THEMES[@]}"; do
+        echo -e "${Y} $key) ${THEMES[$key]}${W}"
+    done
+    echo -e "${Y} 6) Install All Components${W}"
+    echo -e "${R} 0) Exit${W}"
 }
 
-check_deps() {
-    local deps=(wget tar)
-    [ $STYLE -eq 5 ] && deps+=(eww xorg-xrdb git)
+initial_setup() {
+    echo -e "${C}[*] Starting system setup...${W}"
+    pkg update -y && pkg upgrade -y
+    termux-setup-storage
+    pkg install -y x11-repo termux-x11-nightly pulseaudio xfce4 \
+        tur-repo firefox chromium code-oss git wget unzip \
+        meson ninja imagemagick lxappearance
+}
+
+install_deps() {
+    echo -e "${C}[*] Installing dependencies...${W}"
+    pkg install -y zsh python libxml2 glib binutils
+    pip install --user sassc
+}
+
+install_fonts() {
+    echo -e "${C}[*] Installing macOS fonts...${W}"
+    mkdir -p "$FONT_DIR"
+    wget -q --show-progress "https://github.com/samuelngs/apple-emoji-linux/archive/refs/heads/master.zip" -O "apple-fonts.zip"
+    unzip -q apple-fonts.zip -d "$FONT_DIR"
+    rm apple-fonts.zip
+    fc-cache -f -v
+}
+
+install_theme() {
+    local theme="$1"
+    echo -e "${G}[+] Installing ${THEMES[$theme]}...${W}"
     
-    for dep in "${deps[@]}"; do
-        if ! command -v $dep &> /dev/null; then
-            echo -e "${R}[!] Installing $dep...${W}"
-            pkg install -y $dep
+    case $theme in
+        1) # WhiteSur
+            install_whitesur
+            ;;
+        2) # McMojave
+            wget -q --show-progress "${THEME_REPO[McMojave]}" -O "mcmojave.zip"
+            unzip -q mcmojave.zip -d "$THEME_DIR"
+            mv "$THEME_DIR/McMojave-theme-master/McMojave" "$THEME_DIR/McMojave"
+            rm -rf "$THEME_DIR/McMojave-theme-master" mcmojave.zip
+            ;;
+        3) # Cupertino
+            wget -q --show-progress "${THEME_REPO[Cupertino]}" -O "cupertino.zip"
+            unzip -q cupertino.zip -d "$THEME_DIR"
+            mv "$THEME_DIR/Cupertino-gtk-theme-master" "$THEME_DIR/Cupertino"
+            rm cupertino.zip
+            ;;
+        4) # Monterey
+            wget -q --show-progress "${THEME_REPO[Monterey]}" -O "monterey.tar.gz"
+            tar xzf monterey.tar.gz -C "$THEME_DIR"
+            mv "$THEME_DIR/Monterey-kde-master" "$THEME_DIR/Monterey"
+            rm monterey.tar.gz
+            ;;
+        5) # Ventura
+            wget -q --show-progress "${THEME_REPO[Ventura]}" -O "ventura.zip"
+            unzip -q ventura.zip -d "$THEME_DIR"
+            mv "$THEME_DIR/macos-ventura-theme-main" "$THEME_DIR/Ventura"
+            rm ventura.zip
+            ;;
+    esac
+}
+
+install_whitesur() {
+    wget -q --show-progress "${THEME_REPO[WhiteSur]}" -O "whitesur.tar.gz"
+    tar xzf whitesur.tar.gz
+    mv WhiteSur-gtk-theme-master/themes/WhiteSur-Dark "$THEME_DIR"
+    mv WhiteSur-gtk-theme-master/themes/WhiteSur-Dark-solid "$THEME_DIR"
+    rm -rf WhiteSur-gtk-theme-master whitesur.tar.gz
+}
+
+install_icons() {
+    local icon_theme="$1"
+    echo -e "${G}[+] Installing ${icon_theme} icons...${W}"
+    
+    case $icon_theme in
+        "WhiteSur")
+            wget -q --show-progress "${ICON_REPO[WhiteSur]}" -O "whitesur-icons.tar.gz"
+            tar xzf whitesur-icons.tar.gz -C "$ICON_DIR"
+            mv "$ICON_DIR/WhiteSur-icon-theme-master" "$ICON_DIR/WhiteSur"
+            rm whitesur-icons.tar.gz
+            ;;
+        "McMojave")
+            wget -q --show-progress "${ICON_REPO[McMojave]}" -O "mcmojave-icons.zip"
+            unzip -q mcmojave-icons.zip -d "$ICON_DIR"
+            mv "$ICON_DIR/McMojave-circle-master" "$ICON_DIR/McMojave"
+            rm mcmojave-icons.zip
+            ;;
+        "Cupertino")
+            wget -q --show-progress "${ICON_REPO[Cupertino]}" -O "cupertino-icons.zip"
+            unzip -q cupertino-icons.zip -d "$ICON_DIR"
+            mv "$ICON_DIR/Cupertino-mocha-icons-master" "$ICON_DIR/Cupertino"
+            rm cupertino-icons.zip
+            ;;
+    esac
+    
+    gtk-update-icon-cache -f -t "$ICON_DIR/$icon_theme"
+}
+
+setup_wallpapers() {
+    echo -e "${C}[*] Configuring macOS wallpapers...${W}"
+    mkdir -p "$WALLPAPER_DIR"
+    
+    for wall in "${MAC_WALLPAPERS[@]}"; do
+        filename=$(basename "$wall")
+        echo -e "${G}[+] Downloading $filename...${W}"
+        wget -q --show-progress "$wall" -P "$WALLPAPER_DIR"
+        
+        if [[ "$filename" == *".jpeg" ]]; then
+            convert "$WALLPAPER_DIR/$filename" "${WALLPAPER_DIR}/${filename%.*}.png"
+            rm "$WALLPAPER_DIR/$filename"
         fi
     done
 }
 
-install_icons() {
-    echo -e "${C}[*] Installing icon packs...${W}"
-    mkdir -p "$ICON_DIR"
+configure_xfce() {
+    local theme="$1"
+    echo -e "${C}[*] Applying ${THEMES[$theme]} configuration...${W}"
     
-    for pack in "${ICON_PACKS[@]}"; do
-        name=$(basename "$pack" | cut -d'-' -f1)
-        echo -e "${G}[+] Installing $name..."
-        wget -q --show-progress "$pack" -O "$name.tar.gz"
-        tar xzf "$name.tar.gz" -C "$ICON_DIR"
-        rm "$name.tar.gz"
-    done
+    xfconf-query -c xsettings -p /Net/ThemeName -s "${THEMES[$theme]}"
+    xfconf-query -c xsettings -p /Net/IconThemeName -s "WhiteSur"
+    xfconf-query -c xfwm4 -p /general/theme -s "${THEMES[$theme]}"
     
-    gtk-update-icon-cache -f -t "$ICON_DIR"/*
-}
-
-setup_wallpapers() {
-    echo -e "${C}[*] Setting up wallpapers...${W}"
-    mkdir -p "$WALLPAPER_DIR"
-    
-    # Theme wallpapers
-    wget -q --show-progress "${BASE_URL}${STYLE}/wallpaper.tar.gz"
-    tar xzf wallpaper.tar.gz -C "$WALLPAPER_DIR"
-    rm wallpaper.tar.gz
-    
-    # macOS extras
-    if [ $STYLE -eq 3 ]; then
-        for url in "${MACOS_WALLPAPERS[@]}"; do
-            wget -q --show-progress "$url" -P "$WALLPAPER_DIR"
-        done
+    if [ -f "$THEME_DIR/${THEMES[$theme]}/xfce-notify-4.0/gtk.css" ]; then
+        cp "$THEME_DIR/${THEMES[$theme]}/xfce-notify-4.0/gtk.css" "$HOME/.config/gtk-4.0/"
     fi
 }
 
-setup_cyberpunk() {
-    echo -e "${C}[*] Configuring Cyberpunk...${W}"
-    git clone https://github.com/sabamdarif/termux-cyberpunk-theme
-    cp -r termux-cyberpunk-theme/* ~/.config/
-    rm -rf termux-cyberpunk-theme
-}
-
-install_theme() {
-    echo -e "${C}[*] Installing theme components...${W}"
-    mkdir -p "$THEME_DIR"
-    
-    components=(icon theme)
-    for component in "${components[@]}"; do
-        wget -q --show-progress "${BASE_URL}${STYLE}/${component}.tar.gz"
-        tar xzf "${component}.tar.gz" -C "$THEME_DIR"
-        rm "${component}.tar.gz"
-    done
-    
-    [ $STYLE -eq 5 ] && setup_cyberpunk
-}
-
-final_steps() {
+final_config() {
     echo -e "${C}[*] Finalizing setup...${W}"
-    cd ~
-    wget -q --show-progress https://github.com/DeepEyeCrypto/DeepEyeCrypto/raw/refs/heads/main/DeepEyeCrypto.sh
-    chmod +x DeepEyeCrypto.sh
-    echo -e "${Y}[!] Warning: About to run external script DeepEyeCrypto.sh${W}"
-    bash ~/DeepEyeCrypto.sh
+    termux-x11 :0 &
+    sleep 2
+    pulseaudio --start
+    dbus-launch --exit-with-session xfce4-session &
 }
 
 main() {
-    clear
-    echo -e "${C}┌──────────────────────────────────┐"
-    echo -e "│ Termux XFCE Complete Setup Script │"
-    echo -e "└──────────────────────────────────┘${W}"
-    
     initial_setup
-    check_deps
+    install_deps
+    install_fonts
     
-    # Set Zsh as default shell
-    if command -v zsh &>/dev/null; then
-        echo -e "${C}[*] Configuring Zsh...${W}"
-        chsh -s zsh 2>/dev/null || echo -e "${R}Failed to set Zsh as default shell.${W}"
-    fi
-    
-    install_icons
-    setup_wallpapers
-    install_theme
-    final_steps
-    
-    echo -e "\n${C}[√] Installation Complete!"
-    echo -e "${Y}System components:"
-    echo -e " - XFCE4 Desktop Environment"
-    echo -e " - Firefox, Chromium, and Code-OSS"
-    echo -e " - Termux X11 and PulseAudio"
-    echo -e "\nTheme components:"
-    echo -e " - Selected theme: ${THEME_DIR}/xfce-look-${STYLE}"
-    echo -e " - Icon packs: ${ICON_DIR}"
-    echo -e " - Wallpapers: ${WALLPAPER_DIR}"
-    echo -e " - Zsh configured as default shell"
-    echo -e "\n${R}Note: Restart Termux session for all changes to take effect!${W}"
+    while true; do
+        show_menu
+        read -p "Select option (0-6): " choice
+        
+        case $choice in
+            1|2|3|4|5)
+                install_theme $choice
+                install_icons "WhiteSur"
+                setup_wallpapers
+                configure_xfce $choice
+                final_config
+                ;;
+            6)
+                for theme in "${!THEMES[@]}"; do
+                    [ $theme -eq 0 ] && continue
+                    install_theme $theme
+                done
+                for icon in "${!ICON_REPO[@]}"; do
+                    install_icons "$icon"
+                done
+                setup_wallpapers
+                configure_xfce 1  # Default to WhiteSur
+                final_config
+                ;;
+            0)
+                echo -e "${R}Exiting...${W}"
+                exit 0
+                ;;
+            *)
+                echo -e "${R}Invalid option!${W}"
+                ;;
+        esac
+        
+        echo -e "\n${C}[√] ${THEMES[$choice]} installation complete!"
+        echo -e "${Y}Restart XFCE session to see changes"
+        echo -e "Run: xfce4-session${W}\n"
+        read -p "Press enter to continue..."
+    done
 }
 
 main
