d023eec6a942a4f9917f9bb19add66ac28bea020
DeepEyeCrypto <nazzo.np@gmail.com>
2025-03-19 18:00:04 +0530

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,46 +1,38 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 set -e
 
-# Check if running in Termux
-if [ ! -d "$PREFIX" ]; then
-    echo "Error: This script must be run in Termux environment"
-    exit 1
-fi
-
 # Install required packages
-echo -e "\e[1;34mInstalling dependencies...\e[0m"
-pkg update -y && pkg install -y wget tar xz-utils
+pkg install -y wget tar xz-utils
 
 # Create directories
-echo -e "\e[1;34mCreating directories...\e[0m"
-mkdir -p $PREFIX/share/{.icons,.themes,backgrounds/xfce}
+mkdir -p $PREFIX/share/.icons
+mkdir -p $PREFIX/share/.themes
+mkdir -p $PREFIX/share/backgrounds/xfce
 
-# Install icons
-echo -e "\e[1;32mInstalling WhiteSur icons...\e[0m"
-wget -q https://github.com/DeepEyeCrypto/DeepEyeCrypto/raw/main/01-WhiteSur.tar.xz \
-    -O $PREFIX/share/.icons/WhiteSur.tar.xz
-tar -xJf $PREFIX/share/.icons/WhiteSur.tar.xz -C $PREFIX/share/.icons/ && \
-    rm $PREFIX/share/.icons/WhiteSur.tar.xz
+# Download and install icons
+echo "Installing icons..."
+wget -q https://github.com/DeepEyeCrypto/DeepEyeCrypto/raw/main/01-WhiteSur.tar.xz -O $PREFIX/share/.icons/WhiteSur.tar.xz
+tar -xf $PREFIX/share/.icons/WhiteSur.tar.xz -C $PREFIX/share/.icons/
+rm $PREFIX/share/.icons/WhiteSur.tar.xz
 
-# Install themes
-echo -e "\e[1;32mInstalling GTK themes...\e[0m"
-theme_urls=(
-    "WhiteSur-Dark-solid-nord.tar.xz"
-    "WhiteSur-Dark.tar.xz"
-    "WhiteSur-Light.tar.xz"
+# Download and install themes
+echo "Installing themes..."
+THEME_URLS=(
+    "https://github.com/vinceliuice/WhiteSur-gtk-theme/raw/master/release/WhiteSur-Dark-solid-nord.tar.xz"
+    "https://github.com/vinceliuice/WhiteSur-gtk-theme/raw/master/release/WhiteSur-Dark.tar.xz"
+    "https://github.com/vinceliuice/WhiteSur-gtk-theme/raw/master/release/WhiteSur-Light.tar.xz"
 )
 
-for theme in "${theme_urls[@]}"; do
-    echo "Downloading $theme..."
-    wget -q "https://github.com/vinceliuice/WhiteSur-gtk-theme/raw/master/release/$theme" \
-        -O $PREFIX/share/.themes/$theme
-    tar -xJf $PREFIX/share/.themes/$theme -C $PREFIX/share/.themes/
-    rm $PREFIX/share/.themes/$theme
+for url in "${THEME_URLS[@]}"; do
+    filename=$(basename "$url")
+    wget -q "$url" -O $PREFIX/share/.themes/"$filename"
+    tar -xf $PREFIX/share/.themes/"$filename" -C $PREFIX/share/.themes/
+    rm $PREFIX/share/.themes/"$filename"
 done
 
 # Download wallpapers
-echo -e "\e[1;32mDownloading wallpapers...\e[0m"
-wallpaper_urls=(
+echo "Downloading wallpapers..."
+WALLPAPER_URLS=(
     "https://github.com/vinceliuice/WhiteSur-wallpapers/raw/main/4k/Monterey-dark.jpg"
     "https://github.com/vinceliuice/WhiteSur-wallpapers/raw/main/4k/WhiteSur-dark.jpg"
     "https://github.com/vinceliuice/WhiteSur-wallpapers/raw/main/4k/Ventura-dark.jpg"
@@ -51,19 +43,17 @@ wallpaper_urls=(
     "https://4kwallpapers.com/images/wallpapers/sierra-nevada-mountains-macos-high-sierra-mountain-range-5120x2880-8674.jpg"
 )
 
-for wallpaper in "${wallpaper_urls[@]}"; do
-    echo "Downloading $(basename "$wallpaper")..."
-    wget -q --show-progress "$wallpaper" -P $PREFIX/share/backgrounds/xfce/
+for wp_url in "${WALLPAPER_URLS[@]}"; do
+    wget -q --show-progress "$wp_url" -P $PREFIX/share/backgrounds/xfce/
 done
 
-# Cleanup
-echo -e "\e[1;34mCleaning up...\e[0m"
-pkg uninstall -y tar xz-utils
-rm -rf $PREFIX/share/.icons/*.tar.xz $PREFIX/share/.themes/*.tar.xz
+# Clean up
+echo "Cleaning up..."
+apt autoremove -y
 
-echo -e "\e[1;36mInstallation complete!\e[0m"
-echo -e "\nTo apply changes:"
-echo -e "1. Open \e[1;33mXFCE Settings Manager\e[0m"
-echo -e "2. Appearance: Select WhiteSur theme variant"
-echo -e "3. Window Manager: Choose WhiteSur window style"
-echo -e "4. Set wallpaper from $PREFIX/share/backgrounds/xfce/"
+echo "Installation complete!"
+echo "To apply changes:"
+echo "1. Open XFCE Settings Manager"
+echo "2. Choose 'Appearance' to select themes"
+echo "3. Use 'Window Manager' to select window theme"
+echo "4. Set wallpaper from $PREFIX/share/backgrounds/xfce/"
